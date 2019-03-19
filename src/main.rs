14dc4e5fa0cfc2f7a704ed3a9c4eba9ae04061f6
grey <fairingrey@gmail.com>
2019-03-19 11:46:13 -0700

    laying groundwork

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use exitfailure::ExitFailure;
+use std::path::PathBuf;
+use structopt::StructOpt;
+use walkdir::{DirEntry, WalkDir};
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: Option<PathBuf>,
+}
+
+fn main() -> Result<(), ExitFailure> {
+    let args = Cli::from_args();
+
+    let path: PathBuf = match args.path {
+        Some(path) => path,
+        None => PathBuf::from("."),
+    };
+
+    let walker = WalkDir::new(path);
+    for entry in walker {
+        println!("{}", entry?.path().display());
+    }
+    Ok(())
 }

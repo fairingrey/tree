efd51ff49dbf4104cc4f50975433e77bf9884e01
grey <fairingrey@gmail.com>
2019-03-20 11:41:34 -0700

    add lazy static and regex, ignore hidden files

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
+#[macro_use]
+extern crate lazy_static;
+
 use exitfailure::ExitFailure;
 use std::io::prelude::*;
 use std::path::PathBuf;
 use walkdir::{DirEntry, WalkDir};
+use regex::Regex;
 
 pub mod args;
 
+lazy_static! {
+    pub static ref RE_HIDDEN_FILENAME: Regex = Regex::new(r"^\..*$").unwrap();
+}
+
 pub fn write_tree(path: PathBuf, mut handle: impl Write) -> Result<(), ExitFailure> {
-    let walker = WalkDir::new(path);
-    for entry in walker {
+    let walker = WalkDir::new(path).into_iter();
+    for entry in walker.filter_entry(|e| !is_hidden(e)) {
         println!("{}", entry?.file_name().to_str().unwrap());
     }
     Ok(())
+}
+
+pub fn is_hidden(entry: &DirEntry) -> bool {
+    dbg!(entry.file_name());
+    entry.file_name()
+        .to_str()
+        .map(|s| RE_HIDDEN_FILENAME.is_match(s))
+        .unwrap_or(false)
 }
